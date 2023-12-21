3abd7a9dfbf4a2972c8a996c856f4610359357b5
Brad Cypert <brad.cypert@gmail.com>
2023-12-21 15:07:14 -0500

    more docker file changes

--- server/src/hooks.server.ts
+++ server/src/hooks.server.ts
@@ -3,7 +3,10 @@ import GitHub from "@auth/sveltekit/providers/github";
 import {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, AUTH_SECRET} from "$env/static/private";
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET })],
+  providers: [GitHub({ 
+    clientId: GITHUB_CLIENT_ID, 
+    clientSecret: GITHUB_CLIENT_SECRET,
+ })],
   trustHost: true,
   secret: AUTH_SECRET
 })
