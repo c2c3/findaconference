7e64388396e4dbf2b4b1bce6dc2c8bad90c08550
Brad Cypert <brad.cypert@gmail.com>
2023-12-21 10:23:47 -0500

    adjust auth secret env var config

--- server/src/hooks.server.ts
+++ server/src/hooks.server.ts
@@ -1,9 +1,8 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
-import {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, OAUTH_SECRET} from "$env/static/private";
+import {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET} from "$env/static/private";
 
 export const handle = SvelteKitAuth({
   providers: [GitHub({ clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET })],
-  secret: OAUTH_SECRET,
   trustHost: true
 })
