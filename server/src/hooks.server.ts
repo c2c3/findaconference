5fc34d1d24f051c4aa8ca52c2b3265f558f483d0
Brad Cypert <brad.cypert@gmail.com>
2023-12-21 09:46:15 -0500

    small updates for oauth

--- server/src/hooks.server.ts
+++ server/src/hooks.server.ts
@@ -1,7 +1,9 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/sveltekit/providers/github"
+import { SvelteKitAuth } from "@auth/sveltekit";
+import GitHub from "@auth/sveltekit/providers/github";
+import {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, OAUTH_SECRET} from "$env/static/private";
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: process.env.GITHUB_CLIENT_ID, clientSecret: process.env.GITHUB_CLIENT_ID })],
-  secret: process.env.OAUTH_SECRET
+  providers: [GitHub({ clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET })],
+  secret: OAUTH_SECRET,
+  trustHost: true
 })
