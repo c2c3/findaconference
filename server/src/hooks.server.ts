37029d96eaae1f4e938996fb20de7facb90bd5bb
Brad Cypert <brad.cypert@gmail.com>
2023-12-21 12:51:25 -0500

    add another required env var

--- server/src/hooks.server.ts
+++ server/src/hooks.server.ts
@@ -2,9 +2,6 @@ import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
 import {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, AUTH_SECRET} from "$env/static/private";
 
-// Debugging
-console.log("GCI", GITHUB_CLIENT_ID, "GCS", GITHUB_CLIENT_SECRET, "AS", AUTH_SECRET);
-
 export const handle = SvelteKitAuth({
   providers: [GitHub({ clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET })],
   trustHost: true,
