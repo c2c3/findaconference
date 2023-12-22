051fa5b066af142c28d983c9dbc804d687c83124
Brad Cypert <brad.cypert@gmail.com>
2023-12-21 21:24:52 -0500

    Revert "try to skip the CSRF check for auth"
    This reverts commit a2f8b03aaaa379a126cf7d71dc394bc227abbe2e.

--- server/src/hooks.server.ts
+++ server/src/hooks.server.ts
@@ -1,10 +1,8 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
-import { skipCSRFCheck } from "@auth/core";
 import GitHub from "@auth/sveltekit/providers/github";
 import {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, AUTH_SECRET} from "$env/static/private";
 
 export const handle = SvelteKitAuth({
-    skipCSRFCheck: skipCSRFCheck,
   providers: [GitHub({ 
     clientId: GITHUB_CLIENT_ID, 
     clientSecret: GITHUB_CLIENT_SECRET,
